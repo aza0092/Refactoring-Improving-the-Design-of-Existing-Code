2d1f7771a8bd257c5bc7ac6ae9f121b8ed62494d
alali@abdullah-alali.com <31869379+aza0092@users.noreply.github.com>
2019-08-26 19:48:11 -0400

    Add example for Introduce Parameter Object

---
+++ Introduce Parameter Object/index.js
@@ -0,0 +1,31 @@
+/* 
+  This code that looks at a set of temperature readings 
+  and determines whether any of them fall outside of an operating range 
+*/
+
+const station = { name: "ZB1",
+                    readings: [
+                      {temp: 47, time: "2016-11-10 09:10"},
+                      {temp: 53, time: "2016-11-10 09:20"},
+                      {temp: 58, time: "2016-11-10 09:30"},
+                      {temp: 53, time: "2016-11-10 09:40"},
+                      {temp: 51, time: "2016-11-10 09:50"},
+                    ]
+                  };
+/* 
+  This function finds the readings that are outside a temperature range
+*/
+
+function readingsOutsideRange(station, min, max) {
+	return station.readings
+      .filter(r => r.temp < min || r.temp > max);
+  }
+/*
+  It might be called from some code like this:
+*/
+
+caller
+
+alerts = readingsOutsideRange(station,
+                                operatingPlan.temperatureFloor,
+                                operatingPlan.temperatureCeiling);
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Refactoring-Improving-the-Design-of-Existing-Code
-Personal notes from the Book 'Refactoring: Improving the Design of Existing Code by Martin Fowler'
+![Pr-Obj](Pr-Obj.PNG)
+
+# Introduce Parameter Object
+
+## Motivation:
+- Groups of data items that regularly travel together, appearing in function after function is a **data clump** 
+- Replace it with a single data structure
+- Grouping data into a structure is valuable because it makes explicit the relationship between the data items
+- It reduces the size of parameter lists
+
+## Mechanics:
+- Create data structure, usually via a class
+- Use Change Function Declaration to add a parameter for the new structure
+- For each element of the new structure, replace the use of the original parameter with the element of the structure
