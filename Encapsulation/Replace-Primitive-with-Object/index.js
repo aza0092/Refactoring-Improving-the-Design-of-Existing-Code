93c0eeab81f18316478979dbcf0d29decd3c5b87
alali@abdullah-alali.com <31869379+aza0092@users.noreply.github.com>
2019-08-27 00:46:04 -0400

    Replace Primitive with Object

--- Encapsulation/Replace-Primitive-with-Object/index.js
+++ Encapsulation/Replace-Primitive-with-Object/index.js
@@ -1,10 +1,28 @@
-class Order {
-  constructor(data) {
-    this.priority = data.priority;
-    // more initialization
-  }
-
 // some client use it like this: 
-highPriorityCount = orders.filter(o => "high" === o.priority
-                                   || "rush" === o.priority).length;
+highPriorityCount = orders.filter(o => o.priority.higherThan(new Priority("normal"))).length;
 									  
+
+class Order {
+  get priority()        {return this._priority;}
+  get priorityString()  {return this._priority.toString();}
+  set priority(aString) {this._priority = new Priority(aString);}	
+}
+
+
+class Priority {
+  constructor(value) {
+    if (value instanceof Priority) return value;
+    if (Priority.legalValues().includes(value))
+      this._value = value;
+    else
+      throw new Error(`<${value}> is invalid for Priority`);
+  }
+  
+  toString() {return this._value;}
+  get _index() {return Priority.legalValues().findIndex(s => s === this._value);}
+  static legalValues() {return ['low', 'normal', 'high', 'rush'];}
+
+  equals(other) {return this._index === other._index;}
+  higherThan(other) {return this._index > other._index;}
+  lowerThan(other) {return this._index < other._index;}
+}  
