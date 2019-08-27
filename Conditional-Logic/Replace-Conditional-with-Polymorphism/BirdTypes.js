f6cdbb7b3ee0c91037c92440035fa34c4b374782
alali@abdullah-alali.com <31869379+aza0092@users.noreply.github.com>
2019-08-27 02:46:52 -0400

    Combine Functions into class
    We have a couple of different operations that vary with the type of bird, so it makes sense to create classes and use polymorphism for any type-specific behavior.

--- Conditional-Logic/Replace-Conditional-with-Polymorphism/BirdTypes.js
+++ Conditional-Logic/Replace-Conditional-with-Polymorphism/BirdTypes.js
@@ -1,34 +1,39 @@
-  /*
-     My friend has a collection of birds and wants to know 
-	 how fast they can fly and what they have for plumage
-   */
-  function plumages(birds) {
-    return new Map(birds.map(b => [b.name, plumage(b)]));
-  }
-  function speeds(birds) {
-    return new Map(birds.map(b => [b.name, airSpeedVelocity(b)]));
-  }
-  function plumage(bird) {
-    switch (bird.type) {
-    case 'EuropeanSwallow':
-      return "average";
-    case 'AfricanSwallow':
-      return (bird.numberOfCoconuts > 2) ? "tired" : "average";
-    case 'NorwegianBlueParrot':
-      return (bird.voltage > 100) ? "scorched" : "beautiful";
-    default:
-      return "unknown";
+function plumage(bird) {
+    return new Bird(bird).plumage;
+}
+
+
+function airSpeedVelocity(bird) {
+    return new Bird(bird).airSpeedVelocity;
+}
+
+
+class Bird {
+    constructor(birdObject) {
+      Object.assign(this, birdObject);
     }
-  }
-  function airSpeedVelocity(bird) {
-    switch (bird.type) {
-    case 'EuropeanSwallow':
-      return 35;
-    case 'AfricanSwallow':
-      return 40 - 2 * bird.numberOfCoconuts;
-    case 'NorwegianBlueParrot':
-      return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
-    default:
-      return null;
+    get plumage() {
+      switch (this.type) {
+      case 'EuropeanSwallow':
+        return "average";
+      case 'AfricanSwallow':
+        return (this.numberOfCoconuts > 2) ? "tired" : "average";
+      case 'NorwegianBlueParrot':
+        return (this.voltage > 100) ? "scorched" : "beautiful";
+      default:
+        return "unknown";
+      }
     }
-  }
+    get airSpeedVelocity() {
+      switch (this.type) {
+      case 'EuropeanSwallow':
+        return 35;
+      case 'AfricanSwallow':
+        return 40 - 2 * this.numberOfCoconuts;
+      case 'NorwegianBlueParrot':
+        return (this.isNailed) ? 0 : 10 + this.voltage / 10;
+      default:
+        return null;
+      }
+    }
+}
